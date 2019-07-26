cd3f5017c4b8d42ba1bc82ed66ff19575a63c12a
Aniruddha Agarwal <aniruddha.agarwal18@gmail.com>
2019-07-26 09:38:07 +0530

    Name Changes

--- mentor_api/routes/mentorLogin.js
+++ mentor_api/routes/mentorLogin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { loginMentor } = require("../config/function");
+const { loginMentor } = require("../config/loginSignUpFunction");
 const router = express.Router();
 
 router.post("/mentorLogin", async (req, res) => {
