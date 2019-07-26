3add848ddd1b41ecbd02d878e3d60962299992d2
Aniruddha Agarwal <aniruddha.agarwal18@gmail.com>
2019-07-26 09:02:56 +0530

    Format Changes

--- mentor_api/routes/mentorLogin.js
+++ mentor_api/routes/mentorLogin.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const { login } = require('../loginSignupFunction')
+const express = require("express");
+const { login } = require("../loginSignupFunction");
 const router = express.Router();
 
-router.post('/mentorLogin', async (req, res) => {
+router.post("/mentorLogin", async (req, res) => {
     const result = await login(req.body);
-        res.json(result)
+    res.json(result);
 });
 
 module.exports = router;
