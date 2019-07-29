8ac6efa8eed4ee6b2636f90e4c94ed7173eed39a
Aniruddha Agarwal <aniruddha.agarwal18@gmail.com>
2019-07-29 08:52:41 +0530

    Testing

--- course_api/tests/courseRoutesTest.js
+++ course_api/tests/courseRoutesTest.js
@@ -13,3 +13,59 @@ describe('GET /course/table', () => {
             .expect(200, done);
     });
 });
+
+describe('GET /course/id', () => {
+    it('responds with json', (done) => {
+        superTest(app)
+            .get('/course/CH1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
+
+describe('POST /course ', () => {
+    it('adds a course', (done) => {
+        superTest(app)
+            .post('/course')
+            .send({
+                body:{
+                    "courseId": "CHtestRoute",
+                    "courseTitle": "TitleTestRoute",
+                    "courseDescp": "descpTestRoute",
+                    "courseType": "freeTestROute",
+                    "courseCategory": "AItestRoute",
+                },
+            })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
+  
+describe('PUT /course/id', () => {
+    it('updating a course', (done) => {
+        superTest(app)
+            .put('/course/CHtestRoute')
+            .send({
+                body:{
+                    "courseId": "CHtestRoute2",
+                    "courseTitle": "TitleTestRoute2",
+                    "courseDescp": "descpTestRoute2",
+                },
+            })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
+  
+describe('DELETE /course/id ', () => {
+    it('deletes a course', (done) => {
+        superTest(app)
+            .delete('/course/CHtestRoute2')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
